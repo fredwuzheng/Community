e8fb5d68c2a51e21d1df7efd66c90229329ef9f5
Fred Wu Zheng <fredw@berkeley.edu>
2024-05-16 11:40:36 -0700

    add kaptcha to login page

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.nowcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -12,16 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     private UserService userService;
-
+    @Autowired
+    private Producer kaptchaProducer;
     @Value("${server.servlet.context-path}")
     private String contextPath;
 
@@ -68,6 +77,22 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "site/operate-result";
+    }
+    @RequestMapping(path="/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response,HttpSession session){
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+        //save kaptcha code in session
+        session.setAttribute("kaptcha", text);
+        //display image in website
+        response.setContentType("image/png");
+        try{
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        }catch(IOException e){
+            logger.error("error with loading kaptcha:"+ e.getMessage());
+        }
+
     }
 
     @RequestMapping(path ="/login", method = RequestMethod.POST)
