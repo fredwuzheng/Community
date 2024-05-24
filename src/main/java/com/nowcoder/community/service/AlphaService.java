4bc9d1887e5a61a47c0dce7fde69b1a7bbb9f674
Fred Wu Zheng <fredw@berkeley.edu>
2024-05-23 22:13:59 -0700

    tests 2 way of spring Transaction managemant

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,12 +1,25 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype")
@@ -14,7 +27,13 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private TransactionTemplate transactionTemplate;
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
     }
@@ -33,4 +52,63 @@ public class AlphaService {
         return alphaDao.select();
     }
 
+    // REQUIRED: support current transaction(outer transaction), if none create new one
+    // REQUIRES_NEW: create new  transaction , stop transaction(outer transaction)
+    // NESTED: if current transaction(outer transaction) running, it will executed nested within that transaction (independent commit and rollback) otherwise it will act like REQUIRED
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public Object save1(){
+        //create new user
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("alpha@gmail.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        //new user post a post
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello, First post ");
+        post.setContent("This is my first post");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public Object save2(){
+
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status){
+                //create new user
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("alpha@gmail.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/98t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                //new user post a post
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("Hello, I am Beta, First post ");
+                post.setContent("This is my first post");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
+    }
 }
